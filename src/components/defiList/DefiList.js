ffe5e44eabab1c1ecff6bec8c823f872012ad5bb
Booyoun-Kim <booyoun@stamper.network>
2021-05-03 15:59:38 +0900

    added to switch cell function users touch to convert main table(6 <-> 7, 10 <-> 8) and sub table(2 <->3, 5 <-> 4)

--- src/components/defiList/DefiList.js
+++ src/components/defiList/DefiList.js
@@ -291,6 +291,66 @@ const DefiList = observer((props) => {
         return resultImg;
     }
 
+    // 메인페이지의 테이블 셀 스위칭: Token Price <-> MarketCap, TVL <-> holders
+    let isSwitched = false;
+    function switchDefiListTable() {
+        // 모바일 또는 1034 px 이하인 경우
+        let isMobile = false;
+        let advanceSwitchFlag = false;
+
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+            || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+            isMobile = true;
+        }
+
+        if (isMobile) {
+            if (screen.width <= 1034) {
+                advanceSwitchFlag = true;
+            }
+        } else {
+            if (window.innerWidth <= 1034) {
+                advanceSwitchFlag = true;
+            }
+        }
+        
+        // 모바일 뷰, 스크린 조건일 때 스위치 기능 실행
+        if (advanceSwitchFlag) {
+            if (!isSwitched) {
+                document.querySelectorAll('.switchable6').forEach(function(el) {
+                    el.style.display = 'none';
+                });
+                document.querySelectorAll('.switchable7').forEach(function(el) {
+                    el.style.display = 'table-cell';
+                });
+        
+                document.querySelectorAll('.switchable8').forEach(function(el) {
+                    el.style.display = 'table-cell';
+                });
+                document.querySelectorAll('.switchable10').forEach(function(el) {
+                    el.style.display = 'none';
+                });
+
+                isSwitched = true;
+            } else {
+                document.querySelectorAll('.switchable6').forEach(function(el) {
+                    el.style.display = 'table-cell';
+                });
+                document.querySelectorAll('.switchable7').forEach(function(el) {
+                    el.style.display = 'none';
+                });
+        
+                document.querySelectorAll('.switchable8').forEach(function(el) {
+                    el.style.display = 'none';
+                });
+                document.querySelectorAll('.switchable10').forEach(function(el) {
+                    el.style.display = 'table-cell';
+                });
+
+                isSwitched = false;
+            }
+        }
+    }
+
     const [urlFlag1, setUrlFlag1] = useState(false);
 
     async function getDefiList() {
@@ -685,15 +745,15 @@ const DefiList = observer((props) => {
                                 </td>
                                 <td>{tokenSymbolName}</td>
                                 {/* <td>{getOfficialCategoryName(res[i].category)}</td> */}
-                                <td>
+                                <td className="switchable6" onClick={() => switchDefiListTable()}>
                                     <span className="noWrap">{tokenPrice}</span><br />
                                     {tokenPriceChange24hTag}
                                 </td>
-                                <td>
+                                <td className="switchable7" onClick={() => switchDefiListTable()}>
                                     {tokenMarketCapTag}<br />
                                     {tokenMarketCapChange24hTag}
                                 </td>
-                                <td>
+                                <td className="switchable8" onClick={() => switchDefiListTable()}>
                                     {tokenHoldersTag}<br />
                                     {tokenHoldersChange24hNumTag}
                                 </td>
@@ -702,7 +762,7 @@ const DefiList = observer((props) => {
                                     {res[i].volume > 0 ? volumeStr : <div><p data-tip="24hr trading volume hasn't been posted by project team."> {volumeStr}</p><ReactTooltip /></div>}
                                 </td>
                                 {/* <td>$ {numberWithCommas(res[i].lockedUsd)}</td> */}
-                                <td>
+                                <td className="switchable10" onClick={() => switchDefiListTable()}>
                                     <span className="noWrap">$ {currencyNum + currencyUnit}</span><br />
                                     <span className="defiListTableSubText">{change24hTag}</span>
                                 </td>
@@ -819,22 +879,22 @@ const DefiList = observer((props) => {
                             </ul>
                         </th>
                         <th>Token</th>
-                        <th>Token Price</th>
+                        <th className="switchable6">Token Price</th>
                         {/* <th>Contract(#)</th> */}
-                        <th>
+                        <th className="switchable7">
                             <ul className="defiListTableHeadCellRight">
                                 <li>Mkt Cap</li>
                                 {/* <li><span data-tip="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "><img src={questionIcon} /></span><ReactTooltip /></li> */}
                             </ul>
                         </th>
-                        <th>
+                        <th className="switchable8">
                             <ul className="defiListTableHeadCellRight">
                                 <li>Holders</li>
                                 <li><span data-tip="The number of wallets with a balance exceeding zero"><img src={questionIcon} /></span><ReactTooltip /></li>
                             </ul>
                         </th>
                         <th>TVL</th>
-                        <th>
+                        <th className="switchable10">
                             <ul className="defiListTableHeadCellRight">
                                 <li>TVL</li>
                                 <li><span data-tip="Total value locked"><img src={questionIcon} /></span><ReactTooltip /></li>
